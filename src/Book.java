a6d4bd3b4318a66c5e25e26d3a08db4810709d7a
vachg <vachgen@yandex.ru>
2024-09-26 15:57:36 +0300

    objectHW12.2

---
+++ src/Book.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Book {
+    private Author author;
+    private String nameBook;
+    private int year;
+
+    public Book(Author author, String nameBook, int year) {
+        this.author = author;
+        this.nameBook = nameBook;
+        this.year = year;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year < 1805 || year > 1813) {
+            throw new IllegalArgumentException("invalid year");
+
+        }
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(author, book.author) && Objects.equals(nameBook, book.nameBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, nameBook, year);
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО автора: %s\nНазвание книги: %s\nГод публикации: %s".formatted(getAuthor().toString(), getNameBook(), getYear());
+    }
+}
